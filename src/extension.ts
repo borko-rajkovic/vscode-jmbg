39b03a030d903f9a56d6e8958ff586d33c59cec9
Borko Rajkovic <borko.rajkovic@limitforex.com>
2021-02-09 15:09:14 +0100

    Extensions

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,27 @@
 import * as vscode from 'vscode';
 
 import { generateRandomCommand, validateCommand } from './commands';
+import { SidebarProvider } from './SidebarProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-  let validateCommandDisposable = vscode.commands.registerCommand(
-    'vscode-jmbg.validate',
-    validateCommand
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vscode-jmbg.validate', validateCommand)
   );
 
-  context.subscriptions.push(validateCommandDisposable);
-
-  let generateRandomCommandDisposable = vscode.commands.registerCommand(
-    'vscode-jmbg.generate-random',
-    generateRandomCommand
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'vscode-jmbg.generate-random',
+      generateRandomCommand
+    )
   );
 
-  context.subscriptions.push(generateRandomCommandDisposable);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      'vscode-jmbg-sidebar',
+      new SidebarProvider(context.extensionUri),
+      { webviewOptions: { retainContextWhenHidden: true } }
+    )
+  );
 }
 
 export function deactivate() {}
