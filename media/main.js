a929c80b416d563b89c137ab0f377f7d372948a7
Borko Rajkovic <borko.rajkovic@limitforex.com>
2021-02-12 23:38:53 +0100

    Use old state for message

--- media/main.js
+++ media/main.js
@@ -5,26 +5,15 @@
 
   const oldState = vscode.getState();
 
-  const counter = document.getElementById('lines-of-code-counter');
-  console.log(oldState);
-  let currentCount = (oldState && oldState.count) || 0;
-  counter.textContent = currentCount;
-
-  counter.textContent = currentCount++;
-
-  // Update state
-  vscode.setState({ count: currentCount });
+  document.getElementById('p1').innerHTML = oldState.message?.value.text || '';
 
   // Handle messages sent from the extension to the webview
   window.addEventListener('message', (event) => {
     const message = event.data; // The json data that the extension sent
     switch (message.type) {
-      case 'refactor':
-        currentCount = Math.ceil(currentCount * 0.5);
-        counter.textContent = currentCount;
-        break;
       case 'selectedText':
-        document.getElementById('p1').innerHTML = message.value;
+        vscode.setState({ message });
+        document.getElementById('p1').innerHTML = message.value.text;
         break;
     }
   });
