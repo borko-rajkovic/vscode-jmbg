7fb9d90c1c1de3690e24778a735c1cb0aa2f2176
Borko Rajkovic <borko.rajkovic@limitforex.com>
2021-02-14 22:00:24 +0100

    Implement "copy" and "send to editor" buttons

--- media/js/main.js
+++ media/js/main.js
@@ -1,75 +1,69 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
 (function () {
+  const emptyDecoded = {
+    day: null,
+    month: null,
+    year: null,
+    place: null,
+    region: null,
+    gender: null,
+  };
+
+  const emptyMessage = {
+    text: null,
+    valid: false,
+    reason: null,
+    decoded: emptyDecoded,
+  };
+
   const vscode = acquireVsCodeApi();
 
   const oldState = vscode.getState();
 
-  document.getElementById('p1').innerHTML = oldState.message?.value.text || '';
-
   // Handle messages sent from the extension to the webview
   window.addEventListener('message', (event) => {
     const message = event.data; // The json data that the extension sent
-    switch (message.type) {
-      case 'selectedText':
-        vscode.setState({ message });
-        document.getElementById('p1').innerHTML = message.value.text;
-        break;
-    }
+    vscode.setState(message);
+    setAndHighlightElement(message);
   });
 
-  document
-    .getElementById('copyText')
-    .addEventListener('click', copyToClipboard);
+  document.getElementById('btnCopy').addEventListener('click', copyToClipboard);
 
-  document
-    .getElementById('sendToEditor')
-    .addEventListener('click', sendToEditor);
+  document.getElementById('btnPaste').addEventListener('click', sendToEditor);
 
   function copyToClipboard() {
-    var copyText = document.getElementById('p1');
-
     vscode.postMessage({
       type: 'copy',
-      value: copyText.innerHTML,
     });
   }
 
   function sendToEditor() {
-    var copyText = document.getElementById('p1');
-
     vscode.postMessage({
       type: 'sendToEditor',
-      value: copyText.innerHTML,
     });
   }
 
-  let counter2 = 0;
+  function setAndHighlightElement(message) {
+    document.getElementById('codeElement').innerHTML = JSON.stringify(
+      message.decoded,
+      null,
+      2
+    );
 
-  function setAndHighlightElement() {
-    const a = {
-      a: counter2++,
-      b: null,
-      c: 'nesto',
-      d: undefined,
-      e: {
-        e1: 1,
-      },
-      f: [1, 2, 3, 4],
-    };
+    document.getElementById('btnCopy').disabled = !message.valid;
+    document.getElementById('btnPaste').disabled = !message.valid;
 
-    const s = JSON.stringify(a, null, 2);
-
-    document.getElementById('codeElement').innerHTML = s;
-
-    document.querySelectorAll('pre code').forEach((block) => {
-      hljs.highlightBlock(block);
-    });
+    if (!message.valid) {
+      document.getElementById('btnCopyTooltip').classList.remove('is-active');
+      document.getElementById('btnPasteTooltip').classList.remove('is-active');
+    } else {
+      document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+      });
+    }
   }
 
-  setAndHighlightElement();
-
-  setInterval(() => {
-    setAndHighlightElement();
-  }, 200);
+  hljs.initHighlightingOnLoad();
+  setAndHighlightElement(oldState || emptyMessage);
 })();
